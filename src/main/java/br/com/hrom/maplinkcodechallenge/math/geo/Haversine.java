477d51bf82ce48a430b08fedc83e407c1c660dd8
Hromenique Cezniowscki Leite Batista <hromenique@gmail.com>
2017-07-09 13:13:14 -0300

    Alteração de Haversine

--- src/main/java/br/com/hrom/maplinkcodechallenge/math/geo/Haversine.java
+++ src/main/java/br/com/hrom/maplinkcodechallenge/math/geo/Haversine.java
@@ -12,35 +12,19 @@ import br.com.hrom.maplinkcodechallenge.domain.Point;
  */
 public class Haversine {
 	
-	//Raio da Terra em Kilometros
-	private  double R = 6371;
-	
-	private Haversine(){
-		
-	}
-	
-	private Haversine(double r){
-		this.R = r;
-	}
-	
-	/**
-	 * 
-	 * @return uma nova instância de Haversine cujo valor do raio da terra utilizado nos calculos é 6371.0 km
-	 */
-	public static Haversine instance(){
-		return new Haversine();
-	}
-	
-	/**
-	 * 
-	 * @param r distância do raio da terra em quiilomêtros (km) utilizados nos cálculos
-	 * @return uma nova instância de Haversine cujo valor do raio da terra utilizado nos cálculos é definido pelo parâmetro
-	 */
-	public static Haversine instance(double r){
-		return new Haversine(r);
-	}
+	//Raio da Terra em quilometros
+	private static final  double R = 6371;
 
-	public double calculeDistance(double lat1, double lng1, double lat2, double lng2){
+	/**
+	 * 
+	 * @param r raio da terra em quilometros
+	 * @param lat1
+	 * @param lng1
+	 * @param lat2
+	 * @param lng2
+	 * @return
+	 */
+	public static double calculeteDistance(double r, double lat1, double lng1, double lat2, double lng2){
 		double dLat = Math.toRadians(lat2 - lat1);
 		double dLon = Math.toRadians(lng2 - lng1);
 		
@@ -50,10 +34,21 @@ public class Haversine {
         double a = Math.pow(Math.sin(dLat / 2), 2) + Math.pow(Math.sin(dLon / 2),2) * Math.cos(lat1) * Math.cos(lat2);
         double c = 2 * Math.asin(Math.sqrt(a)); 
 		
-        return R * c;
+        return r * c;
 	}
 	
-	public double calculeDistance(Point a, Point b){
+	public static double calculeteDistance(double lat1, double lng1, double lat2, double lng2){
+		return calculeteDistance(R, lat1, lng1, lat2, lng2);
+	}
+	
+	/**
+	 * 
+	 * @param r raio da terra em quilometros
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public static double calculeteDistance(double r, Point a, Point b){
 		Assert.notNull(a, "Point a não pode ser null");
 		Assert.notNull(b, "Point b não pode ser null");
 		
@@ -62,6 +57,10 @@ public class Haversine {
 		double lng1 = a.getLng();
 		double lng2 = b.getLng();
 		
-		return calculeDistance(lat1, lng1, lat2, lng2);
+		return calculeteDistance(r, lat1, lng1, lat2, lng2);
+	}
+	
+	public static double calculeteDistance(Point a, Point b){
+		return calculeteDistance(R, a, b);
 	}
 }
--- src/test/java/br/com/hrom/maplinkcodechallenge/math/geo/HaversineTest.java
+++ src/test/java/br/com/hrom/maplinkcodechallenge/math/geo/HaversineTest.java
@@ -17,7 +17,7 @@ public class HaversineTest {
 		double lat2 = 47.6788206;
 		double lng2 = -122.5271205;
 		
-		double distance = Haversine.instance().calculeDistance(lat1, lng1, lat2, lng2);
+		double distance = Haversine.calculeteDistance(lat1, lng1, lat2, lng2);
 		
 		assertEquals(14.973190481586224, distance, 0);		
 	}
@@ -27,7 +27,7 @@ public class HaversineTest {
 		Point a = new Point(47.6788206, -122.3271205);
 		Point b = new Point(47.6788206, -122.5271205);
 		
-		double distance = Haversine.instance().calculeDistance(a, b);
+		double distance = Haversine.calculeteDistance(a, b);
 		
 		assertEquals(14.973190481586224, distance, 0);
 	}
@@ -36,12 +36,12 @@ public class HaversineTest {
 	public void calculaDistanciaDePontosIguais(){
 		double lat = 47.6788206;
 		double lng = -122.3271205;
-		double distance = Haversine.instance().calculeDistance(lat, lng, lat, lng);
+		double distance = Haversine.calculeteDistance(lat, lng, lat, lng);
 		assertEquals(0, distance, 0);	
 		
 		Point a = new Point(lat, lng);
 		Point b = new Point(lat, lng);
-		distance = Haversine.instance().calculeDistance(a, b);
+		distance = Haversine.calculeteDistance(a, b);
 		assertEquals(0, distance, 0);	
 	}
 	
@@ -53,16 +53,14 @@ public class HaversineTest {
 		double lng1 = -77.037852;
 		
 		double lat2 = 38.897147;
-		double lng2 = -77.043934;
+		double lng2 = -77.043934;		
 		
-		Haversine haversine = Haversine.instance(novoRaio);
-		
-		double distance = haversine.calculeDistance(lat1, lng1, lat2, lng2);
+		double distance = Haversine.calculeteDistance(novoRaio, lat1, lng1, lat2, lng2);
 		assertEquals(0.5493281835413389, distance, 0);
 		
 		Point a = new Point(lat1, lng1);
 		Point b = new Point(lat2, lng2);
-		distance = haversine.calculeDistance(a, b);
+		distance = Haversine.calculeteDistance(novoRaio, a, b);
 		assertEquals(0.5493281835413389, distance, 0);		
 	}
 
